7081a90432f295744fb7f2ca7d197e0c84b522c6
ianwyt <mihailsmirnov2511@gmail.com>
2023-04-24 02:28:14 +0300

    added search bar functionality

--- src/app/components/navbar-search/navbar-search.component.ts
+++ src/app/components/navbar-search/navbar-search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-search',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar-search.component.css']
 })
 export class NavbarSearchComponent {
+  constructor(private router: Router) { }
 
+  search(query: string) {
+    const lowerCaseQuery = query.toLowerCase();
+
+    switch (lowerCaseQuery) {
+      case 'blogs':
+        this.router.navigate(['/blogs']);
+        break;
+      case 'signin':
+        this.router.navigate(['/signin']);
+        break;
+      case 'signup':
+        this.router.navigate(['/signup']);
+        break;
+      case 'testimonials':
+        this.router.navigate(['/testimonials']);
+        break;
+      case 'about':
+        this.router.navigate(['/about']);
+        break;
+      case 'newsletter':
+        this.router.navigate(['/newsletter']);
+        break;
+      default:
+        this.router.navigate(['/page-not-found']);
+        break;
+    }
+  }
 }
