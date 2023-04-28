970ae2322d24bf5a9c3e83eae72991838c21791e
ianwyt <mihailsmirnov2511@gmail.com>
2023-04-28 15:43:18 +0300

    burger menu problem fixed but only for one navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  @ViewChild('burgerMenu') burgerMenu!: ElementRef;
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  toggleBurgerMenu(): void {
+    this.burgerMenu.nativeElement.classList.toggle('hidden');
+  }
+
+  collapseBurgerMenu(): void {
+    if (!this.burgerMenu.nativeElement.classList.contains('hidden')) {
+      this.burgerMenu.nativeElement.classList.add('hidden');
+    }
+  }
 }
