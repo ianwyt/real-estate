0fc4578186ba0f185de0ef63f80883233d7613eb
ianwyt <mihailsmirnov2511@gmail.com>
2023-05-15 13:22:46 +0300

    new component, upload photo feature added

--- src/app/pages/showroom/showroom.component.ts
+++ src/app/pages/showroom/showroom.component.ts
@@ -3,10 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'showroom-app',
     template: `
-        <app-navbar-search></app-navbar-search>
-        <app-http-codes></app-http-codes>
-        <app-newsletter></app-newsletter>
-        <app-footer></app-footer>
+        <app-upload-photo></app-upload-photo> 
     `
 })
 export class ShowroomComponent { }
